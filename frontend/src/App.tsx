d930f401c00fc201258072dbe4fbd93da7556649
karir <c1406274@st.kanazawa-it.ac.jp>
2025-02-24 23:28:50 +0900

    一時保存

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import TopPage from './pages/TopPage/TopPage';
 import { HelmetProvider } from "react-helmet-async";
 
 const App: React.FC = () => {
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/TopPage" element={<TopPage />} />
         </Routes>
       </Router>
     </HelmetProvider>
