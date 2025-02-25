a7b774809222491f9054fa573c7691ab7c0e6183
karir <c1406274@st.kanazawa-it.ac.jp>
2025-02-26 01:37:51 +0900

    WelcomePageの作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import TopPage from './pages/TopPage/TopPage';
 import { HelmetProvider } from "react-helmet-async";
+import WelcomePage from './pages/WelcomePage/WelcomePage';
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/TopPage" element={<TopPage />} />
+          <Route path="/WelcomePage" element={<WelcomePage />} />
         </Routes>
       </Router>
     </HelmetProvider>
