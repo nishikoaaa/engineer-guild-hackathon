f4908429700255c624f45bff49c741c01b173989
nishikoaaa <c1304499@st.kanazawa-it.ac.jp>
2025-02-27 20:03:05 +0900

    terminal_operationの追加

---
+++ backend/app/terminal_operation.py
@@ -0,0 +1,12 @@
+import subprocess
+import time
+
+while True:
+    # file1.py を実行
+    subprocess.run(["python", "/app/app/url_acquisition.py"], check=True)
+    print("url_acuisition.pyを実行しました。")
+    # file2.py を実行
+    subprocess.run(["python", "/app/app/embedd.py"], check=True)
+    print("embedd.pyを実行しました。")
+    # 24時間待機（86400秒）
+    time.sleep(86400)
